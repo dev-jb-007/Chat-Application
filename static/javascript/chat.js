2fc11689f31ab705c5ac27af0fd5d04143e18504
Dev Patel <devpatel8907@gmail.com>
2021-08-13 20:43:11 +0530

    Changing from main os to ssd os 13 aug 8:43pm

--- static/javascript/chat.js
+++ static/javascript/chat.js
@@ -1,16 +1,30 @@
 let socket=io();
 console.log('hi');
-let chatbox=document.getElementById('chatbox');
+let mainchats=document.getElementById('main-chats');
+let Othermessages=chatbox.querySelector('.chat-display-messages');
 let chatboxHtml=``;
 const {username,room}=Qs.parse(location.search, { ignoreQueryPrefix: true });
 socket.emit('join',{username,room})
-socket.on('welcomeGreeting',(message)=>{
-    console.log('welcome');
+socket.on('welcomeGreeting',(Obj)=>{
+    chatboxHtml=`<div class="chat-display-messages">
+        <p>${Obj.text}</p>
+        <ion-icon class="back-carret" name="caret-back-outline"></ion-icon>
+    </div>`
     // chatboxHtml+=`<p>${message}</p>`
+    mainchats.innerHTML=chatboxHtml;
+})
+socket.on('userjoined',(Obj)=>{
+    chatboxHtml+=`<div class="chat-display-messages">
+        <p>${Obj.username} Joined!!!</p>
+        <ion-icon class="back-carret" name="caret-back-outline"></ion-icon>
+    </div>`;
+    mainchats.innerHTML=chatboxHtml;
     // chatbox.innerHTML=chatboxHtml;
 })
-socket.on('userjoined',(obj)=>{
-    console.log('hi');
-    // chatboxHtml+=`<span>${obj.username} joined</span> -<span>${moment(obj.createdAt).format('h:m a')}</span>`
-    // chatbox.innerHTML=chatboxHtml;
+socket.on('userleft',(Obj)=>{
+    chatboxHtml+=`<div class="chat-display-messages">
+        <p>${Obj.username} Left!!!</p>
+        <ion-icon class="back-carret" name="caret-back-outline"></ion-icon>
+    </div>`;
+    mainchats.innerHTML=chatboxHtml;
 })
